bca1a86afe55cd51b5eaf4cbc906ba3125d29026
Virjilakrum <asesenep15@gmail.com>
2024-07-28 18:24:33 +0300

    static-target wasm

---
+++ target/debug/deps/file_read-5359880a9d0f4e71.d
@@ -0,0 +1,5 @@
+/Users/dante/Downloads/rust-wasm-master/file-read/target/debug/deps/libfile_read-5359880a9d0f4e71.rmeta: src/main.rs
+
+/Users/dante/Downloads/rust-wasm-master/file-read/target/debug/deps/file_read-5359880a9d0f4e71.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/file_read-9b4b4347027a54f3.d
@@ -0,0 +1,5 @@
+/Users/dante/Downloads/rust-wasm-master/file-read/target/debug/deps/libfile_read-9b4b4347027a54f3.rmeta: src/main.rs
+
+/Users/dante/Downloads/rust-wasm-master/file-read/target/debug/deps/file_read-9b4b4347027a54f3.d: src/main.rs
+
+src/main.rs:
